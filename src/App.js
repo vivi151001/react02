64db324fa92e9768fb40cdefbae0b161c2047170
vivi151001 <137512572+vivi151001@users.noreply.github.com>
2023-10-17 18:03:46 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import C01componente from './pagina/C01componente';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <C01componente/>
+      Hola mundo desde react...
+      
     </div>
   );
 }
