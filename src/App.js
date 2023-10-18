a93c8f3ee459d59350fe47bc4aeec9960efe2d6a
vivi151001 <137512572+vivi151001@users.noreply.github.com>
2023-10-18 18:18:54 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import C01componente from './pagina/C01componente';
+import AppForm from './pagina/AppForm';
 
 function App() {
   return (
-    <div>
-      <C01componente/>
-      Hola mundo desde react...
-      <i class="medium material-icons">insert_chart</i>
+    <div style={{width:"350px",background:"greenyellow", padding:"10px"}}>
+    <AppForm />
+    <p>Nª. 1 Juan Manuel         x    A </p>
+    <p>Nª. 2 Rosa Maria         x    A </p>
     </div>
   );
 }
